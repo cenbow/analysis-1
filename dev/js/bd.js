4fc70115fec8e8d14a8c81f6d76cf2d3c615ae95
pumpe zhu <zzplxm1988@gmail.com>
2017-05-17 15:59:43 +0800

    调整目录结构

--- dev/js/bd.js
+++
@@ -1,5 +0,0 @@
-/* Baidu */
-var protocol = document.location.protocol == 'https:' ? 'https://' : 'http://';
-var baidu = document.createElement('script');
-baidu.src = protocol + 'hm.baidu.com/h.js?87035a00e917f0eee43e4967b495f7c9';
-document.body.appendChild(baidu);
